1efb2028721e02b31873ced1098739a46315ea8d
Victor Diez <victordc239@gmail.com>
2025-06-26 14:01:52 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -3,30 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: victor <victor@student.42.fr>              +#+  +:+       +#+        */
+/*   By: vdiez-cu <vdiez-cu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/08 12:54:15 by victor            #+#    #+#             */
-/*   Updated: 2025/06/25 17:16:46 by victor           ###   ########.fr       */
+/*   Updated: 2025/06/26 13:47:02 by vdiez-cu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
-
-void	ft_close(t_pipex *p, int num_close)
-{
-	if (num_close == 4)
-	{
-		close(p->out_in_cmd_pipe[0]);
-		close(p->out_in_cmd_pipe[1]);
-		close(p->infile_fd);
-		close(p->outfile_fd);
-	}
-	else if (num_close == 2)
-	{
-		close(p->infile_fd);
-		close(p->outfile_fd);
-	}
-}
 
 void	arguments(int argc, char **argv, t_pipex *p)
 {
@@ -40,8 +24,8 @@ void	arguments(int argc, char **argv, t_pipex *p)
 			p->count++;
 		if (p->count != 4)
 		{
-			write(2, "Error format\n", 14);
-			exit(EXIT_FAILURE);
+			ft_free_split(p->argv_split);
+			(write(2, "Error format\n", 14), exit(EXIT_FAILURE));
 		}
 		p->infile = p->argv_split[0];
 		p->cmd1_str = p->argv_split[1];
@@ -70,14 +54,15 @@ void	first_child(t_pipex *p, char **envp)
 			ft_perror_exit("dup2 pipe write cmd1", 0, p);
 		ft_close(p, 4);
 		p->cmd1_split = ft_split(p->cmd1_str, ' ');
-		if (!p->cmd1_split)
-			exit(EXIT_FAILURE);
+		if (p->cmd1_split[0] == NULL)
+			(ft_free_split(p->cmd1_split), exit(EXIT_FAILURE));
 		p->path_cmd1 = find_executable(p->cmd1_split[0], envp);
 		if (!p->path_cmd1)
 		{
 			write(2, "command not found: ", 19);
 			write(2, p->cmd1_split[0], ft_strlen(p->cmd1_split[0]));
 			write(2, "\n", 1);
+			ft_free_split(p->cmd1_split);
 			exit(EXIT_FAILURE);
 		}
 		execve(p->path_cmd1, p->cmd1_split, envp);
@@ -98,44 +83,66 @@ void	second_child(t_pipex *p, char **envp)
 			ft_perror_exit("dup2 outfile cmd2", 0, p);
 		ft_close(p, 4);
 		p->cmd2_split = ft_split(p->cmd2_str, ' ');
-		if (!p->cmd2_split)
-			exit(EXIT_FAILURE);
+		if (p->cmd2_split[0] == NULL)
+			(ft_free_split(p->cmd2_split), exit(EXIT_FAILURE));
 		p->path_cmd2 = find_executable(p->cmd2_split[0], envp);
 		if (!p->path_cmd2)
 		{
 			write(2, "command not found: ", 19);
 			write(2, p->cmd2_split[0], ft_strlen(p->cmd2_split[0]));
 			write(2, "\n", 1);
-			exit(EXIT_FAILURE);
+			ft_free_split(p->cmd2_split);
+			exit(127);
 		}
 		execve(p->path_cmd2, p->cmd2_split, envp);
 		ft_perror_exit("execve cmd2", 0, p);
 	}
 }
 
+void	open_files(t_pipex *p, int *outfile_error)
+{
+	p->infile_fd = open(p->infile, O_RDONLY);
+	if (p->infile_fd < 0)
+	{
+		perror("open infile");
+		p->infile_fd = open("/dev/null", O_RDONLY);
+		if (p->infile_fd < 0)
+			ft_perror_exit("open /dev/null", 0, p);
+	}
+	p->outfile_fd = open(p->outfile, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+	if (p->outfile_fd < 0)
+	{
+		perror("open outfile");
+		*outfile_error = 1;
+		p->outfile_fd = open("/dev/null", O_WRONLY);
+		if (p->outfile_fd < 0)
+			ft_perror_exit("open /dev/null", 1, p);
+	}
+}
+
 int	main(int argc, char *argv[], char *envp[])
 {
 	t_pipex	p;
+	int		status1;
+	int		status2;
+	int		outfile_error;
 
+	outfile_error = 0;
 	if (argc == 2 || argc == 5)
 		arguments(argc, argv, &p);
 	else
-	{
-		write(1, "Error\nArguments\n", 17);
-		exit(EXIT_FAILURE);
-	}
-	p.infile_fd = open(p.infile, O_RDONLY);
-	if (p.infile_fd < 0)
-		ft_perror_exit("open infile", 0, &p);
-	p.outfile_fd = open(p.outfile, O_CREAT | O_WRONLY | O_TRUNC, 0644);
-	if (p.outfile_fd < 0)
-		ft_perror_exit("open outfile", 1, &p);
+		(write(1, "Error\nArguments\n", 17), exit(EXIT_FAILURE));
+	open_files(&p, &outfile_error);
 	if (pipe(p.out_in_cmd_pipe) < 0)
 		ft_perror_exit("pipe", 2, &p);
-	first_child(&p, envp);
-	second_child(&p, envp);
-	ft_close(&p, 4);
-	waitpid(p.pid1, NULL, 0);
-	waitpid(p.pid2, NULL, 0);
-	return (0);
+	(first_child(&p, envp), second_child(&p, envp), ft_close(&p, 4));
+	(waitpid(p.pid1, &status1, 0), waitpid(p.pid2, &status2, 0));
+	if (argc == 2)
+		ft_free_split(p.argv_split);
+	if (outfile_error)
+		return (1);
+	if (WIFEXITED(status2))
+		return (WEXITSTATUS(status2));
+	else
+		return (1);
 }
--- pipex_utils.c
+++ pipex_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipex_utils.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: victor <victor@student.42.fr>              +#+  +:+       +#+        */
+/*   By: vdiez-cu <vdiez-cu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/08 13:22:12 by victor            #+#    #+#             */
-/*   Updated: 2025/06/25 17:16:54 by victor           ###   ########.fr       */
+/*   Updated: 2025/06/26 13:47:11 by vdiez-cu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,4 +84,20 @@ void	ft_perror_exit(char *str, int num_close, t_pipex *p)
 	}
 	perror(str);
 	exit(EXIT_FAILURE);
+}
+
+void	ft_close(t_pipex *p, int num_close)
+{
+	if (num_close == 4)
+	{
+		close(p->out_in_cmd_pipe[0]);
+		close(p->out_in_cmd_pipe[1]);
+		close(p->infile_fd);
+		close(p->outfile_fd);
+	}
+	else if (num_close == 2)
+	{
+		close(p->infile_fd);
+		close(p->outfile_fd);
+	}
 }
